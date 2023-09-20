bc54bb87189bc9172aca340cca8280ec23405cd0
Joan-Njiru <joanwanjirunjiru@example.com>
2023-09-20 13:07:10 +0300

    added media queries

--- frontend-dashboard/app/Components/Display/index.tsx
+++ frontend-dashboard/app/Components/Display/index.tsx
@@ -1,52 +1,42 @@
-''
-import React from "react"
-import {SiGoogleclassroom} from 'react-icons/si';
-import {CgNotes} from 'react-icons/cg';
-import {PiChalkboardTeacherDuotone} from 'react-icons/pi';
-import {PiStudent} from 'react-icons/pi';
-import {RiParentLine} from 'react-icons/ri';
+import React from "react";
+import { SiGoogleclassroom } from 'react-icons/si';
+import { CgNotes } from 'react-icons/cg';
+import { PiChalkboardTeacherDuotone } from 'react-icons/pi';
+import { PiStudent } from 'react-icons/pi';
+import { RiParentLine } from 'react-icons/ri';
 
+let data = [
+    { icon: <SiGoogleclassroom />, user: "Classes", number: "58" },
+    { icon: <CgNotes />, user: "Subjects", number: "12" },
+    { icon: <PiChalkboardTeacherDuotone />, user: "Teachers", number: "120" },
+    { icon: <PiStudent />, user: "Student", number: "932" },
+    { icon: <RiParentLine />, user: "Parents", number: "932" },
+];
 
-
-
-
-
-let data =[
-    {icon:<SiGoogleclassroom/>,user:"Classes",
-     number:"58"},
-     {icon:<CgNotes/>,user:"Subjects",
-     number:"12"},
-     {icon:<PiChalkboardTeacherDuotone/>,user:"Teachers",
-     number:"120"},
-     {icon:<PiStudent/>,user:"Student",
-     number:"932"},
-     {icon:<RiParentLine/>,user:"Parents",
-     number:"932"},
-]
-const Display =()=>{
-    return(
-       <section className="rounded-full border-black ">
-            <div className="flex mx-20 bg-blue-400 rounded mt-40">
-                <div className="w-1/2 p-2 h-44">
-                    <h1 className="text-white m-2 text-2xl font-bold ">Welcome to Utawala Primary School <br /> School Dashboard</h1>
-                    <p className="text-gray-200 text-sm">Track your school activities in one place</p>
+const Display = () => {
+    return (
+        <section className="rounded-full border-black md:mr-6 sm:mx-2">
+            <div className="flex flex-col sm:flex-row mx-2 md:mx-20 bg-blue-500 rounded mt-4 md:mt-20 sm:mt-0">
+                <div className="w-full sm:w-1/2 p-2 h-auto md:h-44">
+                    <h1 className="text-white p-4 md:p-8 text-2xl font-bold ">Welcome to Utawala Primary <br /> School Dashboard</h1>
+                    <p className="text-gray-200 p-2 md:pl-8 text-sm">Track your school activities in one place</p>
                 </div>
-                <div className="w-1/2">
-                    <img className="h-300 ml-60" src="media/dashboard.png" alt="" />
+                <div className="w-full sm:w-1/2 md:w-1/2 md:float-right">
+                    <img className="h-300 ml-0 sm:ml-0 md:ml-0 md:mr-60" src="media/dashboard.png" alt="" />
                 </div>
             </div>
-            <div className="flex items-center my-10 ml-20 ">
-                <input className=" border border-gray-300 py-4 w-1/2" type="text" placeholder="    search for class/teacher/student..." />
+            <div className="flex items-center my-4 sm:my-10 ml-2 md:ml-20 ">
+                <input className="border border-gray-300 py-2 sm:py-4 w-full md:w-1/2" type="text" placeholder="Search for class/teacher/student..." />
             </div>
-            <div className="flex gap-14 mx-20 ">
+            <div className="flex flex-col sm:flex-row gap-2 md:gap-16 mx-2 md:mx-20 ">
                 {data.map((item, index) => (
-                    <div className="py-12 px-16 flex bg-gray-200" key={index} >
-                        <div className="rounded-full bg-blue-500 p-6 mr-4 ">
-                            <i className="rounded-full text-3xl text-white">{item.icon}</i>
+                    <div className="py-6 sm:py-12 px-4 md:px-16 flex bg-gray-200" key={index} >
+                        <div className="rounded-full bg-blue-500 p-2 sm:p-12 mr-4 md:mr-4 ">
+                            <i className="rounded-full text-2xl md:text-3xl text-white  ">{item.icon}</i>
                         </div>
                         <div>
                             <h2 className="text-gray-400">{item.user}</h2> <br />
-                            <p className="font-extrabold text-3xl text-navy">{item.number}</p>
+                            <p className="font-extrabold text-xl md:text-3xl text-navy">{item.number}</p>
                         </div>
                     </div>
                 ))}
