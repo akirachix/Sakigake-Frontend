7431cc1f023b8815c99e7c3fd3ba6d3fe4269189
Joan-Njiru <joanwanjirunjiru@example.com>
2023-09-27 11:46:53 +0300

    styled dashboard page

--- frontend-dashboard/app/dashboard/page.tsx
+++ frontend-dashboard/app/dashboard/page.tsx
@@ -6,17 +6,17 @@ import { PiStudent } from 'react-icons/pi';
 import { RiParentLine } from 'react-icons/ri';
 
 let data = [
-    { icon: <SiGoogleclassroom className=" bg-bgblue rounded-full text-6xl py-2.5 text-white"/>, user: "Classes", number: "58" },
-    { icon: <CgNotes className=" bg-bgblue rounded-full text-6xl py-2.5 text-white" />, user: "Subjects", number: "12" },
-    { icon: <PiChalkboardTeacherDuotone className=" bg-bgblue rounded-full text-6xl py-2.5 text-white"/>, user: "Teachers", number: "120" },
-    { icon: <PiStudent className=" bg-bgblue rounded-full text-6xl py-2.5 text-white"/>, user: "Students", number: "932" },
-    { icon: <RiParentLine className=" bg-bgblue rounded-full text-6xl py-2.5 text-white" />, user: "Parents", number: "932" },
+    { icon: <SiGoogleclassroom className=" bg-bgblue rounded-full text-6xl py-2.5 mt-6 text-white"/>, user: "Classes", number: "58" },
+    { icon: <CgNotes className=" bg-bgblue rounded-full text-6xl py-2.5 text-white  mt-6" />, user: "Subjects", number: "12" },
+    { icon: <PiChalkboardTeacherDuotone className=" bg-bgblue rounded-full text-6xl mt-6 py-2.5 text-white"/>, user: "Teachers", number: "120" },
+    { icon: <PiStudent className=" bg-bgblue rounded-full text-6xl py-2.5  mt-6 text-white"/>, user: "Students", number: "932" },
+    { icon: <RiParentLine className=" bg-bgblue rounded-full text-6xl py-2.5  mt-6 text-white" />, user: "Parents", number: "932" },
 ];
 
 const Display = () => {
     return (
-        <section className="flex ">
-            <div className="w-5/6">
+        <section className="flex ml-14 ">
+            <div >
             <div className="flex flex-col mr-10 rounded-2xl sm:flex-row mt-0  bg-bgblue rounded-0  md:mt-24 mb-10 sm:mt-0">
                 <div className="w-full sm:w-1/2 ">
                     <h1 className="text-white p-8 leading-relaxed text-4xl font-bold ">Welcome to Utawala Primary <br /> School Dashboard</h1>
@@ -31,11 +31,11 @@ const Display = () => {
             </div>
             <div className="flex flex-col mr-10 mt-4 py-20 sm:flex-row md:gap-6">
                 {data.map((item, index) => (
-                    <div className="py-6 text-2xl px-12 sm:py-8 gap-8 flex bg-grey" key={index} >
+                    <div className="py-6 text-2xl px-12 sm:py-8 gap-10 flex bg-grey" key={index} >
                         {item.icon}
                         <div>
-                            <h2 className="text-textgrey">{item.user}</h2> <br />
-                            <p className="font-extrabold  md:text-3xl text-navyblue">{item.number}</p>
+                            <h2 className="text-textgrey text-xl">{item.user}</h2> <br />
+                            <p className="font-extrabold  md:text-4xl text-navyblue">{item.number}</p>
                         </div>
                     </div>
                 ))}
